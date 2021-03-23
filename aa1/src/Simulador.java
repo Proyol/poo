d5ca34ca707e7b2f3ba2e1771763444fa1e87987
Daniel <danielfavero17@gmail.com>
2021-03-23 17:34:15 -0300

    refactor and docs: added documentation and code refactoring 📝

--- aa1/src/Simulador.java
+++ aa1/src/Simulador.java
@@ -1,30 +1,37 @@
-import java.io.Console;
 import java.util.Scanner;
 
 public class Simulador {
-    private static Scanner teclado = new Scanner(System.in);
+    // array de veículos com o total de 20 veiculos
     private static Veiculo[] veiculos = new Veiculo[20];
 
     public static void main(String[] args) {
         menu();
     }
 
+    // função para pausar a execução da aplicação
+    // usada para o usuário poder ler o que tem em tela antes de prosseguir com a
+    // continuação do programa
     public static void pausarExecucao() {
         Scanner pausar = new Scanner(System.in);
-        String pausa = pausar.nextLine();
+        pausar.nextLine();
     }
 
+    // função para solicitar o id do veiculo dentro do array
     public static int solicitarID() {
-        System.out.print("\nInforme o id do carro: ");
-        int id = teclado.nextInt();
+        System.out.print("\nInforme o id do veiculo: ");
 
+        Scanner teclado = new Scanner(System.in);
+        int id = teclado.nextInt(); // usuário informa o id
+
+        // retornar o id digitado pelo usuário
         return id;
     }
 
+    // função para mostrar o menu de opções do simulador para o usuário
     public static void menu() {
-
         int opcao = 0;
 
+        // mostrar o menu enquanto o usuário não informar o opção de sair
         while (opcao != 11) {
             System.out.println("1) Incluir veículo");
             System.out.println("2) Remover veículo");
@@ -39,6 +46,8 @@ public class Simulador {
             System.out.println("11) Sair");
             System.out.print("Opção: ");
 
+            // ler a opção do menu
+            Scanner teclado = new Scanner(System.in);
             opcao = teclado.nextInt();
 
             switch (opcao) {
@@ -75,7 +84,7 @@ public class Simulador {
                 break;
             }
             case 9: {
-                calibrarTodosPneus();
+                calibrarTodasPneusDeTodosVeiculos();
                 break;
             }
             case 10: {
@@ -83,15 +92,17 @@ public class Simulador {
                 break;
             }
             case 11: {
-                System.exit(1);
+                sair();
                 break;
             }
             default: {
                 System.out.println("Opção inválida, tente novamente");
-                pausarExecucao();
             }
             }
 
+            pausarExecucao();
+
+            // limpar a tela toda vez que o menu é mostrado novamente
             System.out.print("\033[H\033[2J");
             System.out.flush();
         }
@@ -100,112 +111,184 @@ public class Simulador {
     public static void incluirVeiculo() {
         int i;
 
+        // incluir veiculos no array
         for (i = 0; i < veiculos.length; i++) {
+
+            // incluir o veiculo caso haja posição disponível
             if (veiculos[i] == null) {
-                veiculos[i] = new Veiculo();
-                veiculos[i].iniciarVeiculo(i);
+                veiculos[i] = new Veiculo(i);
+                veiculos[i].iniciarVeiculo();
+
+                // forçar saida do for
                 i = veiculos.length;
+
+                // mostrar mensagem de sucesso para o usuário
                 System.out.println("\nVeículo adicionado com sucesso");
             }
         }
+
+        // caso não for encontrado posição disponível, ou seja, i nunca é igual a
+        // veiculos.length, avisar o usuário que não possível a inserção do veículo
         if (i < veiculos.length) {
             System.out.println("\nNão foi possível adicionar o veículo");
         }
-
-        pausarExecucao();
     }
 
     public static void excluirVeiculo() {
+        // solicitar o id do veiculo a ser excluido pelo usuário
         int id = solicitarID();
 
+        // verificar se o veiculo existe no array
         if (veiculos[id] != null && id >= 0 && id < veiculos.length) {
+
+            // caso tenha encontrado
             veiculos[id] = null;
             System.out.println("\nVeículo removido com sucesso");
         } else {
+
+            // caso NÃO tenha encontrado
             System.out.println("\nNão foi possível remover o veículo");
         }
-        pausarExecucao();
     }
 
     public static void abastecerVeiculo() {
+        // usuário deve informar a quantidade de combustível a abastecer
+        Scanner teclado = new Scanner(System.in);
         double quantidade = teclado.nextFloat();
+        teclado.close();
+
+        // solicitar o id do veiculo a ser abastecido
         int id = solicitarID();
         if (veiculos[id] != null) {
+
+            // caso o veiculo for encontrado
             veiculos[id].abastecer(quantidade);
             System.out.println("\nVeículo abastecido com sucesso");
         } else {
+
+            // caso o veiculo NÃO for encontrado
             System.out.println("\nNão foi possível abastecer o veículo");
         }
-        pausarExecucao();
     }
 
     public static void movimentarVeiculo() {
+        // solicitar o id do veiculo que deve ser movido
         int id = solicitarID();
+
+        // verificar se o veículo foi movimento e validar se sua movimentação foi bem
+        // sucedida
         if (veiculos[id] != null && veiculos[id].movimentar()) {
+
+            // veículo se movimentou com sucesso
             System.out.println("\nVeiculo movido com sucesso");
         } else {
+
+            // veículo não conseguiu se movimentar
             System.out.println("\nNão foi possível movimentar o veiculo");
         }
-        pausarExecucao();
     }
 
     public static void movimentarTodosVeiculos() {
+        // percorrer todos os veículos e eventualmente movimenta-los
         for (Veiculo veiculo : veiculos) {
             if (veiculo != null) {
                 veiculo.movimentar();
             }
         }
-        System.out.println("\nVeículos movidos com sucesso");
-        pausarExecucao();
+
+        System.out.println("\nVeiculos movimentados com sucesso");
     }
 
     public static void imprimirDadosVeiculos() {
+        // percorrer todos os veículos e imprimir todos os seus dados
         for (Veiculo veiculo : veiculos) {
             if (veiculo != null) {
                 System.out.println(veiculo);
             }
         }
-        pausarExecucao();
     }
 
     public static void esvaziarPneuDeVeiculo() {
+        Scanner teclado = new Scanner(System.in);
+        int pneu;
+
+        // solicitar o id do veículo a esvaziar os pneus
         int id = solicitarID();
+
+        // verificar se o veículo existe
         if (veiculos[id] != null) {
-            veiculos[id].esvaziarPneus();
-            System.out.println("\nRodas esvaziadas com sucesso");
+            System.out.print("\nInforme a pneu a ser esvaziada: ");
+            pneu = teclado.nextInt();
+
+            // verificar se a pneu informada é válida
+            if (pneu >= 0 && pneu < 4) {
+                veiculos[id].esvaziarPneu(pneu);
+
+                // pneu esvaziado com sucesso
+                System.out.println("\nPneus esvaziadas com sucesso");
+            } else {
+
+                // NÃO foi possível esvaziar os pneus
+                System.out.println("\nInforme um valor válido para as Pneus");
+            }
+
         } else {
-            System.out.println("\nNão foi possível esvaziadar as rodas");
+
+            // NÃO foi possível esvaziar os pneus
+            System.out.println("\nNão foi possível esvaziadar as Pneus");
         }
-        pausarExecucao();
     }
 
     public static void calibrarPneusDeVeiculo() {
+        Scanner teclado = new Scanner(System.in);
+        int pneu;
+
+        // solicitar o id do veículo a calibrar os pneus
         int id = solicitarID();
+
+        // verificar se o veículo existe
         if (veiculos[id] != null) {
-            veiculos[id].calibrarPneus();
-            System.out.println("\nPneus Calibradas com sucesso");
+            System.out.print("\nInforme a pneu a ser esvaziada: ");
+            pneu = teclado.nextInt();
+
+            if (pneu >= 0 && pneu < 4) {
+                veiculos[id].esvaziarPneu(pneu);
+
+                // Pneus calibrados com sucesso
+                System.out.println("\nPneus calibradas com sucesso");
+            } else {
+                // NÃO foi possível esvaziar os pneus
+                System.out.println("\nInforme um valor válido para as Pneus");
+            }
         } else {
+
+            // NÃO foi possível calibrar os pneus
             System.out.println("\nNão foi possível calibrar os pneus");
         }
-        pausarExecucao();
     }
 
-    public static void calibrarTodosPneus() {
+    public static void calibrarTodasPneusDeTodosVeiculos() {
+        // percorrer todos os veículos e calibrar o pneu de cada um
         for (Veiculo veiculo : veiculos) {
             if (veiculo != null) {
-                veiculo.calibrarPneus();
+                veiculo.calibrarTodasOsPneus();
             }
         }
-        System.out.println("\nPneus dos veiculos calibrados com sucesso");
-        pausarExecucao();
+
+        System.out.println("\nPneus calibradas com sucesso");
     }
 
     public static void imprimirPista() {
+        // desenhar todos os veiculo dentro do array
         for (Veiculo veiculo : veiculos) {
             if (veiculo != null) {
                 veiculo.desenhar();
             }
         }
-        pausarExecucao();
+    }
+
+    public static void sair() {
+        // sair da aplicação
+        System.exit(1);
     }
 }
--- aa1/src/Veiculo.java
+++ aa1/src/Veiculo.java
@@ -1,113 +1,161 @@
 import java.util.*;
 
+// classe veículo, o simulador poderá ter no máximo 20 veículos
 public class Veiculo {
-    private int id;
+    private int id; // id do veículo
     private int n; // número de pneus
-    private Roda[] rodas;
-    private double combustivel;
-    private double valor;
-    private boolean ipvaPago;
+    private Roda[] rodas; // rodas do veículo
+    private double combustivel; // quantidade de combustível do veículo
+    private double valor; // valor do veículo
+    private boolean ipvaPago; // controlar se o ipva está pago ou não
+    private int blocosAndados; // quantidade de blocos andados
 
-    public Veiculo() {
+    // método construtor para iniciar todas as variáveis da classe
+    public Veiculo(int id) {
         this.id = 0;
         this.n = 0;
         this.combustivel = 0;
         this.valor = 0;
         this.ipvaPago = false;
+        this.blocosAndados = 0;
     }
 
+    // função para retornar o id do veículo
     public int getId() {
         return this.id;
     }
 
+    // função para iniciar a calibragem dos pneus
     private void iniciarPneus() {
         int i;
-        Random r = new Random();
+        Random r = new Random(); // classe Random do java.util
 
+        // percorrer todas as rodas do veículo
         for (i = 0; i < n; i++) {
-            this.rodas[i] = new Roda();
+            this.rodas[i] = new Roda(); // criar nova roda
             int c = r.nextInt(100);
 
+            // caso o valor de c for par o pneu é calibrado, caso contrário não
             if (c % 2 == 0) {
                 this.rodas[i].setCalibragem(true);
             }
         }
     }
 
-    public void iniciarVeiculo(int id) {
-        this.id = id;
+    // função para setar todas as variáveis do veículo para valores padrões
+    public void iniciarVeiculo() {
+        this.combustivel = 3.5; // combustível em litros
+        this.valor = 20000; // valor em R$
 
-        this.combustivel = 3.5;
-        this.valor = 20000;
-
+        // iniciar as rodas
         this.n = 4;
         this.rodas = new Roda[n];
         iniciarPneus();
 
+        // iniciar o ipva
+        // caso de r for par o ipva está pago, caso contrário não
         Random r = new Random();
         this.ipvaPago = r.nextInt(100) % 2 == 0 ? true : false;
     }
 
+    // função para abastecer o veículo
     public void abastecer(double quantidade) {
         this.combustivel += quantidade;
     }
 
+    // função que verificar se o veículo pode moder
+    // um veículo poderá se mover se:
+    // 1 - Todas os pneus estiverem calibradas
+    // 2 - Possir combustível suficiente (cada movimento reduz 0,55L do tanque)
+    // 3 - O ipva estiver pago
     private boolean podeMover() {
-        boolean rodasCalibradas = true;
+        boolean pneusCalibrados = true;
 
-        // verificar se todas as rodas estão calibrar
+        // verificar se todas os pneus estão calibrar
         for (Roda roda : this.rodas) {
             if (!roda.getCalibragem()) {
-                rodasCalibradas = false;
+                pneusCalibrados = false;
             }
         }
 
-        return this.combustivel - 0.55 >= 0 && this.ipvaPago && rodasCalibradas;
+        return this.combustivel - 0.55 >= 0 && this.ipvaPago && pneusCalibrados;
     }
 
+    // função para movimentar o veículo
     // a função retorna verdadeiro caso o movimento for bem sucedido
     public boolean movimentar() {
         if (podeMover()) {
+            // descontar 0,55L do tanque
             this.combustivel -= 0.55;
+
+            // aumentar a quantidade de blocos andados
+            this.blocosAndados += 5;
+
+            // movimento foi bem sucedido
             return true;
         }
 
+        // movimento não pôde ser realizado
         return false;
     }
 
-    public void esvaziarPneus() {
-        for (Roda roda : rodas) {
-            roda.setCalibragem(false);
-        }
+    // função para esvaziar um pneu do veículo
+    // o pneu a ser esvaziada é passado por parâmetro
+    public void esvaziarPneu(int roda) {
+        // percorrer todas as rodas e esvaziar os pneus
+        this.rodas[roda].setCalibragem(false);
     }
 
-    public void calibrarPneus() {
+    // função para calibrar um pneu do veículo
+    // o pneu a ser calibrada é passado por parâmetro
+    public void calibrarPneu(int roda) {
+        this.rodas[roda].setCalibragem(true);
+    }
+
+    // função para calibrar os pneus do veículo
+    public void calibrarTodasOsPneus() {
+        // percorrer todas as rodas e calibrar os pneus
         for (Roda roda : rodas) {
             roda.setCalibragem(true);
         }
     }
 
+    // função para desenhar o veículo na linha de comando
     public void desenhar() {
-        System.out.println("    ___  ");
-        System.out.println(" __/ |_\\_");
-        System.out.println("|  _ " + this.id + "  _‘‘-.");
-        System.out.println("’-(_)---(_)--’\n");
+        int i;
+        String espacos = "";
+
+        for (i = 0; i < this.blocosAndados; i++)
+            espacos += " ";
+
+        System.out.println(espacos + "    ___  ");
+        System.out.println(espacos + " __/ |_\\_");
+        System.out.println(espacos + "|  _ " + this.id + "  _‘‘-.");
+        System.out.println(espacos + "’-(_)---(_)--’\n");
     }
 
+    // função para imprimir as informações do veículo
     public String toString() {
-        String s = "\nid: " + this.id + "\n" + "Valor: " + this.valor + "\n" + "Combustível: " + this.combustivel
-                + "\n";
+        // id
+        String s = "\nid: " + this.id + "\n";
 
+        // valor
+        s += "Valor: " + this.valor + "\n";
+
+        // combustível
+        s += "Combustível: " + this.combustivel + "\n";
+
+        // ipva pago
         String ipvaEstaPago = this.ipvaPago ? "Pago\n" : "Não pago\n";
         s += "IPVA: " + ipvaEstaPago;
 
+        // calibragem dos pneus
         int i;
-        String rodasCalibradas = "Rodas Calibradas: ";
+        String pneusCalibrados = "Pneus Calibradas: ";
         for (i = 0; i < n; i++) {
-            rodasCalibradas += this.rodas[i].getCalibragem() ? "1 " : "0 ";
+            pneusCalibrados += this.rodas[i].getCalibragem() ? "1 " : "0 ";
         }
-        s += rodasCalibradas;
-        s += "\n";
+        s += pneusCalibrados + '\n';
 
         return s;
     }
